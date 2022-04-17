115e553b906baf433f5f0505338f95fe6f177aa5
carternadain <cnadain@gmail.com>
2022-04-17 10:24:47 -0700

    add sql connection

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // const listed here
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const util = require('util');
 
 
+// sql connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: '',
+  database: 'election'
+});
